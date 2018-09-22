57ba7b0e4f72d067ed00ce36c5d7835ae1c03001
Wilhson <Wilhomega@hotmail.com>
2018-09-22 20:43:48 +0200

    efe

--- main.py
+++ main.py
@@ -1,36 +1,4 @@
-from tools import *
-import pygame
-#HEEY
-from entity import *
-from tools import *
-from player import *
-from background import *
-
-pygame.mixer.pre_init(44100, -16, 2, 2048)  #bug soundp
-pygame.init()
-
-window = pygame.display.set_mode((tools.w0, tools.h0), pygame.FULLSCREEN)
-
-from textbox import *
-from load import *
-
-# Set icon
-pygame.display.set_icon(icon)
-
-# Set title
-pygame.display.set_caption("HatHome")
-
-# Set Music
-pygame.mixer.music.load("Assets\Music\music.mp3")
-pygame.mixer.music.play(-1)
-pygame.mixer.music.set_volume(4)
-
-# Tick Init
-clock = pygame.time.Clock()
-
-# Create player
-home = Player(750, 400, spriteplayer[0], 100, 50, spriteplayer)
-background = Background(5, background, 0, 0)
+from init import *
 # Main loop
 run = True
 while run:
